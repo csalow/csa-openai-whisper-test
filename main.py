3529a6d8e8b31bd9410acbc32aeafa105ec59529
csalow <develop@salownet.de>
2024-01-02 12:53:47 +0100

    .

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=openai_api_key)
 
-# audio_file= open("audio/audio-sample-2.wav", "rb")
 audio_file= open("audio/Urlaub-in-den-Bergen.mp3", "rb")
 transcript = client.audio.transcriptions.create(
   model="whisper-1", 
